0064eac83b12d1e6465320c8e3e5236d7c7891e2
anhld <wolfwithcode@gmail.com>
2020-05-29 17:05:23 +0700

    Look good structure for skill pages

--- src/components/main/about-detail-item/about-detail-item.component.js
+++ src/components/main/about-detail-item/about-detail-item.component.js
@@ -5,7 +5,7 @@ export default function AboutDetailItem(props) {
             <div className="about-me__about-detail">             
                 <div className="row">
                     <div className="col-md-2"><img className="about-me__icon" src={props.icon}/></div>
-                    <div className="col-md-8 offset-sm-1">
+                    <div className="col-md-8 offset-md-1">
                         <h4 className="about-me__about-detail-title">{props.title}</h4>
                         <p className="summary__about-detail-description">{props.description}</p>
                     </div>   
--- src/pages/skillspage/skillspage.component.js
+++ src/pages/skillspage/skillspage.component.js
@@ -29,16 +29,16 @@ export default class SkillsPage extends Component {
     render(){
         return (
             <section className="my-skills">
-                <div className="container">
+                <div className="container-fuild">
                     <div className="row">
-                        <div className="col">
+                        <div className="col-md-4 offset-md-2">
                             <div className="my-skills__content">
                                 <h3 className="my-skills__title">MY SKILLS</h3>
                                 <p className="my-skills__description">{this.state.my_skill_summary_data}</p>
                                 <button className="btn btn-success" type="button">Contact me</button>
                             </div>
                         </div>
-                        <div className="col">
+                        <div className="col-md-4">
                             <div className="container">    
                                 {
                                     this.state.my_skill_items_data.map((dataObject, index) => (
@@ -47,6 +47,16 @@ export default class SkillsPage extends Component {
                                 } 
                                                             
                             </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="text-center">
+                            <div class="col-md-8 offset-md-2">
+                                <h3 class="my-skills__resume_title">RESUME</h3>
+                                <p class="my-skills__resume_description">Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Proin gravida nibh vel velit auctor aliquet. Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Proin gravida nibh vel velit auctor aliquet. Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. </p>
+                                <button class="btn btn-success" type="button">DOWNLOAD RESUME</button>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
